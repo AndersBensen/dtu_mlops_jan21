54e39d8b411da3e441faa6dddb42cd02248cce9b
AndersBensen <anders_bensen@hotmail.com>
2022-01-10 13:54:41 +0100

    updated actions pipeline

--- day2/m5_m6_m7_m8/dtu_mlops_jan21/tests/test_data.py
+++ day2/m5_m6_m7_m8/dtu_mlops_jan21/tests/test_data.py
@@ -4,28 +4,41 @@ import numpy as np
 import pytest
 import os.path
 
-train_set = MnistDataset('data/processed/train_tensor.pt')
-test_set = MnistDataset('data/processed/test_tensor.pt')
-
-
+# Test data size
 @pytest.mark.skipif(not os.path.exists('data/processed/train_tensor.pt'), reason="Data files not found")
 def test_train_data():
+    train_set = MnistDataset('data/processed/train_tensor.pt')
     assert len(train_set) == 25000, "Traning dataset did not have the correct number of samples"
 
 @pytest.mark.skipif(not os.path.exists('data/processed/test_tensor.pt'), reason="Data files not found")
 def test_test_data():
+    test_set = MnistDataset('data/processed/test_tensor.pt')
     assert len(test_set) == 5000, "Testing datataset did not have the correct number of samples"   
 
-
 # Test data shapes
-rand_tens = torch.rand(28,28)
-for i in train_set: assert i[0].shape == rand_tens.shape, "All samples in training dataset not have the right shape" 
-for i in test_set: assert i[0].shape == rand_tens.shape , "All samples in training dataset not have the right shape"
+@pytest.mark.skipif(not os.path.exists('data/processed/train_tensor.pt'), reason="Data files not found")
+def test_train_shapes():
+    rand_tens = torch.rand(28,28)
+    train_set = MnistDataset('data/processed/train_tensor.pt')
+    for i in train_set: assert i[0].shape == rand_tens.shape, "All samples in training dataset not have the right shape" 
 
-# Test labels
-train_labels = [int(i[1]) for i in train_set]
-test_labels = [int(i[1]) for i in test_set]
+@pytest.mark.skipif(not os.path.exists('data/processed/test_tensor.pt'), reason="Data files not found")
+def test_test_shapes():
+    rand_tens = torch.rand(28,28)
+    test_set = MnistDataset('data/processed/test_tensor.pt')
+    for i in test_set: assert i[0].shape == rand_tens.shape , "All samples in testing dataset not have the right shape"
 
-numbers = set(range(0,10))
-assert set(train_labels) == numbers, "All the labels were not found in the training dataset"
-assert set(test_labels) == numbers, "All the labels were not found in the test dataset"
+# Test data labels
+@pytest.mark.skipif(not os.path.exists('data/processed/train_tensor.pt'), reason="Data files not found")
+def test_train_labels():
+    train_set = MnistDataset('data/processed/train_tensor.pt')
+    train_labels = [int(i[1]) for i in train_set]
+    numbers = set(range(0,10))
+    assert set(train_labels) == numbers, "All the labels were not found in the training dataset"
+
+@pytest.mark.skipif(not os.path.exists('data/processed/test_tensor.pt'), reason="Data files not found")
+def test_test_labels():
+    test_set = MnistDataset('data/processed/test_tensor.pt')
+    numbers = set(range(0,10))
+    test_labels = [int(i[1]) for i in test_set]
+    assert set(test_labels) == numbers, "All the labels were not found in the test dataset"
