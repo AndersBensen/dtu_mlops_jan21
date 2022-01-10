3a122bcf507b9e67e129f924e9ae21c1ab4927d6
AndersBensen <anders_bensen@hotmail.com>
2022-01-10 13:56:46 +0100

    updated actions pipeline

--- day2/m5_m6_m7_m8/dtu_mlops_jan21/tests/test_training.py
+++ day2/m5_m6_m7_m8/dtu_mlops_jan21/tests/test_training.py
@@ -3,27 +3,32 @@ from src.data.dataset import MnistDataset
 import torch 
 from torch import nn, optim
 import numpy as np
+import os
+import pytest
 
-train_set = MnistDataset('data/processed/train_tensor.pt')
 
-model = MyAwesomeModel()
+@pytest.mark.skipif(not os.path.exists('data/processed/train_tensor.pt'), reason="Data files not found")
+def test_train_labels():
+    train_set = MnistDataset('data/processed/train_tensor.pt')
 
-criterion = nn.NLLLoss()    
-optimizer = optim.Adam(model.parameters(), lr=float(3e-5))
+    model = MyAwesomeModel()
 
-image, label = train_set[0]
+    criterion = nn.NLLLoss()    
+    optimizer = optim.Adam(model.parameters(), lr=float(3e-5))
 
-log_ps = model(image.unsqueeze(0).float())
-loss = criterion(log_ps, label.unsqueeze(0))
-loss.backward()
+    image, label = train_set[0]
 
-optimizer.step()
+    log_ps = model(image.unsqueeze(0).float())
+    loss = criterion(log_ps, label.unsqueeze(0))
+    loss.backward()
 
-weights_pre_zg = model.conv_1.weight.grad.view(-1)
+    optimizer.step()
 
-assert len(set(weights_pre_zg.tolist())) > 1, "The gradients were all zero before zero gradding"
+    weights_pre_zg = model.conv_1.weight.grad.view(-1)
 
-optimizer.zero_grad()
+    assert len(set(weights_pre_zg.tolist())) > 1, "The gradients were all zero before zero gradding"
 
-weights_after_zg = model.conv_1.weight.grad.view(-1)
-assert len(set(weights_pre_zg.tolist())) == 1, "The gradients were all NOT zero after zero gradding"
+    optimizer.zero_grad()
+
+    weights_after_zg = model.conv_1.weight.grad.view(-1)
+    assert len(set(weights_pre_zg.tolist())) == 1, "The gradients were all NOT zero after zero gradding"
